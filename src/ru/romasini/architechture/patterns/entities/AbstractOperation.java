66104d8d8531de45e1f20e72d33927bc184e6906
Roman Sinitsyn <romasini@gmail.com>
2021-01-04 17:13:57 +0300

    декоратор

--- src/ru/romasini/architechture/patterns/entities/AbstractOperation.java
+++ src/ru/romasini/architechture/patterns/entities/AbstractOperation.java
@@ -1,14 +1,17 @@
 package ru.romasini.architechture.patterns.entities;
 
+import ru.romasini.architechture.patterns.decorator.OperationComponent;
+
 import java.util.Date;
 
-public abstract class AbstractOperation {
+public abstract class AbstractOperation implements OperationComponent {
 
     protected Long id;
     protected Date date;
     protected User user;
     protected Source source;
     protected Double amount;
+    protected String comment = new String();
 
     public Long getId() {
         return id;
@@ -50,4 +53,11 @@ public abstract class AbstractOperation {
         this.amount = amount;
     }
 
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
